c20c5f30724630d0611b8a6c73fa7353e9bd00da
shu00011 <koha.koishi.gnu.16@gmail.com>
2021-08-07 00:17:15 +0900

    [ADD] created Header

---
+++ components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+  <header>
+    <div class="header_flex">
+  <!-- データを表示させる-->
+      <h1>{{title}}</h1>
+      <nav>
+        <ul>
+          <li>profile</li>
+          <li>works</li>
+        </ul>
+      </nav>
+      <!--<p>{{subTitle}}</p>-->
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      title:"shu00011",
+      //subTitle:"portfolio"
+    }
+  },
+  name: "Header"
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Geo&display=swap');
+  header{
+    background-color: #f5f5f5;
+    text-align:center;
+    padding:10px;
+    font-family:'Geo', sans-serif;
+    font-weight: lighter;
+  }
+  h1{
+    color: #a9a9a9;
+    margin:0;
+    font-family:'Geo', sans-serif;
+    font-weight: lighter;
+  }
+  p{
+    color:#000;
+    margin:0;
+  }
+  ul{
+    list-style:none;
+  }
+  div{
+    padding:0 30px;
+  }
+  .header_flex{
+    display:flex;
+    height:60px;
+    align-items:center;
+  }
+  .header_flex nav{
+    margin-left:auto;
+  }
+  .header_flex ul{
+    display:flex;
+  }
+  .header_flex ul li{
+    margin-left:20px;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+<!--  <Tutorial/>-->
+  <div>
+    <Header/>
+    <p>{{title}}</p>
+    <hello-world message='This is a message.'/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "~/components/HelloWorld";
+import Header from "~/components/Header";
+export default {
+  data:function(){
+    return{
+      title:'Hello, World!!',
+    }
+  },
+  components:{
+    Header,
+    HelloWorld
+  }
+}
+</script>
